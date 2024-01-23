e99d2f0625ebe5de233f510f93659cc157dfb813
Student <student@Mac.local>
2024-01-23 08:31:54 -0800

    added calculator

--- HelloWorld/HelloWorld/MainPage.xaml.cs
+++ HelloWorld/HelloWorld/MainPage.xaml.cs
@@ -4,22 +4,22 @@ public partial class MainPage : ContentPage
 {
 
 
-	public MainPage()
-	{
-		InitializeComponent();
-	}
+    public MainPage()
+    {
+        InitializeComponent();
+    }
 
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
+    private void OnCounterClicked(object sender, EventArgs e)
+    {
         if (lblDisplay.Text == "Hello World")
-		{
-			lblDisplay.Text = "Good Day";
-		}
-		else
-		{
-			lblDisplay.Text = "Hello World";
-		}
-	}
+        {
+            lblDisplay.Text = "Good Day";
+        }
+        else
+        {
+            lblDisplay.Text = "Hello World";
+        }
+    }
 }
 
 
