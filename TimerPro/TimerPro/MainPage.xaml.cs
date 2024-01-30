e3482ea8fb17078bfb27270c365e5dc993880f49
Student <student@Mac.local>
2024-01-30 08:21:26 -0800

    finished timer

---
+++ TimerPro/TimerPro/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using TimerPro.custom;
+
+namespace TimerPro;
+
+public partial class MainPage : ContentPage
+{
+    TimerLogic oTimerLogic = new TimerLogic();
+    bool isRunning = false;
+	public MainPage()
+	{
+		InitializeComponent();
+		Title = "TimerPro";
+	}
+
+    void btnStart_Clicked(System.Object sender, System.EventArgs e)
+    {
+        btnStart.IsEnabled = false;
+        btnStop.IsEnabled = true;
+        isRunning = true;
+
+        Dispatcher.StartTimer(TimeSpan.FromSeconds(1), () => {
+            if (isRunning)
+            {
+                oTimerLogic.SetTickCount();
+                lblDisplay.Text = oTimerLogic.GetFormattedString();
+            }
+            return isRunning;
+        });
+
+    }
+
+    void btnStop_Clicked(System.Object sender, System.EventArgs e)
+    {
+        btnStop.IsEnabled = false;
+        btnStart.IsEnabled = true;
+        isRunning = false;
+    }
+
+    void btnReset_Clicked(System.Object sender, System.EventArgs e)
+    {
+        oTimerLogic.Reset();
+        lblDisplay.Text = oTimerLogic.GetFormattedString();
+    }
+}
+
+
