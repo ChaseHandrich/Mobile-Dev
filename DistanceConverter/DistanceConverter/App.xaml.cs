aa02d70454e8a32550bf08852d75679040c28e61
Student <student@Mac.local>
2024-02-20 08:44:37 -0800

    finished distance converter

---
+++ DistanceConverter/DistanceConverter/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using DistanceConverter.views;
+
+namespace DistanceConverter;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new NavigationPage(new StartPage());
+	}
+}
+
---
+++ DistanceConverter/DistanceConverter/views/AboutPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace DistanceConverter.views;
+
+public partial class AboutPage : ContentPage
+{
+	public AboutPage()
+	{
+		InitializeComponent();
+	}
+}
